a8b7747211d9a149604788f6f5561c2be6834e0f
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-07 10:45:44 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab1._1 {
+    class Program {
+        static void Main(string[] args) {
+            string output = "";
+            while (output.Equals(q)) {
+                output = Console.ReadLine();
+                Console.WriteLine(output);
+            }
+        }//static void Main(string[] args) {
+    }//class Program {
+}//namespace Lab1._1 {
