059f9f1773f26682ec0d409ac93ae4427bc2a290
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2021-06-07 10:54:19 -0500

    Final

--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@
 namespace Lab1._1 {
     class Program {
         static void Main(string[] args) {
-            string output = "";
-            while (output.Equals(q)) {
-                output = Console.ReadLine();
-                Console.WriteLine(output);
-            }
+            Console.WriteLine("What is your name?");
+            string output1 = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string output2 = Console.ReadLine();
+            Console.WriteLine("Hello, " + output1 +" you are "+output2+" years old.");
         }//static void Main(string[] args) {
     }//class Program {
 }//namespace Lab1._1 {
